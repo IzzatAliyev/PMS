d5698c18343abd69913d1ba585b1f30b1025f5fa
Izzat Aliyev <izzat@izzats-mbp.speedport.ip>
2023-04-17 04:36:37 +0200

    added upload media in employee controller

--- PMS.Web/Controllers/EmployeeController.cs
+++ PMS.Web/Controllers/EmployeeController.cs
@@ -1,3 +1,4 @@
+using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc;
 using PMS.Service.ViewModels.Employee;
 using PMS.Service.ViewModels.Project;
@@ -27,7 +28,7 @@ namespace PMS.Web.Controllers
                 {
                     var response2 = await client.GetAsync($"employee-project/employee/{id}/projects");
                     var projects = await response2.Content.ReadFromJsonAsync<IEnumerable<ProjectViewModel>>();
-                    ViewBag.Projects = projects; 
+                    ViewBag.Projects = projects;
                     var response3 = await client.GetAsync($"employee-skill/employee/{id}/skills");
                     var skills = await response3.Content.ReadFromJsonAsync<IEnumerable<SkillViewModel>>();
                     ViewBag.Skills = skills;
@@ -56,6 +57,21 @@ namespace PMS.Web.Controllers
                 {
                     return this.RedirectToAction("NotFound", "Home");
                 }
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadMedia(IFormFile mediaFile)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5275/api2/");
+                var formData = new MultipartFormDataContent();
+                var fileContent = new StreamContent(mediaFile.OpenReadStream());
+                fileContent.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+                formData.Add(fileContent, "mediaFile", mediaFile.FileName);
+                var response = await client.PostAsync("mediums", formData);
+                return response.IsSuccessStatusCode ? RedirectToAction("Index", new { id = 1} ) : RedirectToAction("SomethingWentWrong", "Home");
             }
         }
     }
